21718a12893ad13c2fddc518166dd608e669f033
Kevin Schmid <schmid.kevin.manuel@gmail.com>
2025-08-04 13:05:15 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -15,11 +15,9 @@
       nixpkgs.config.allowUnfree = true;
       environment.systemPackages = with pkgs; [
           gnupg
-          vim
-          home-manager
-          neovim
           nerd-fonts.cousine
           fzf
+          neovim
           yq
           ripgrep
           azure-cli
@@ -58,11 +56,6 @@
         shell = pkgs.bash;
       };
       environment.shellAliases = {
-        ls = "ls --color=auto";
-        l = "ls -lah";
-        v = "nvim";
-        g = "git";
-        k = "kubectl";
         nixrebuild = "sudo darwin-rebuild switch --flake ~/.config/nix";
       };
 
@@ -112,100 +105,10 @@
         "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
       };
 
-      # top bar
-      services.sketchybar.enable = true;
-      services.sketchybar.config = ''
-PLUGIN_DIR="/Users/kevin.schmid/.config/sketchybar/plugins"
-sketchybar --bar position=top height=40 blur_radius=30 color=0xFF000000
-sketchybar --default \
-  padding_left=5 \
-  padding_right=5 \
-  icon.font="Cousine Nerd Font:Bold:17.0" \
-  label.font="Cousine Nerd Font:Bold:14.0" \
-  icon.color=0xffffffff \
-  label.color=0xffffffff \
-  icon.padding_left=4 \
-  icon.padding_right=4 \
-  label.padding_left=4 \
-  label.padding_right=4
-sketchybar --add event aerospace_workspace_change
-for sid in $(aerospace list-workspaces --all); do
-    sketchybar --add item space.$sid left \
-        --subscribe space.$sid aerospace_workspace_change \
-        --set space.$sid \
-        label="$sid" \
-        click_script="aerospace workspace $sid" \
-        script="$PLUGIN_DIR/aerospace.sh $sid"
-done
-sketchybar --add item chevron left \
-           --set chevron label.drawing=off \
-           --add item front_app left \
-           --set front_app icon.drawing=off script="$PLUGIN_DIR/front_app.sh" \
-           --subscribe front_app front_app_switched
-sketchybar --add item clock right \
-           --set clock update_freq=10 icon=  script="$PLUGIN_DIR/clock.sh" \
-           --add item volume right \
-           --set volume script="$PLUGIN_DIR/volume.sh" \
-           --subscribe volume volume_change \
-           --add item battery right \
-           --set battery update_freq=120 script="$PLUGIN_DIR/battery.sh" \
-           --subscribe battery system_woke power_source_change
-sketchybar --update
-      '';
       services.jankyborders.enable = true;
       services.jankyborders.hidpi = true;
       services.jankyborders.active_color = "gradient(top_left=0xFF6EC3B4,bottom_right=0xFFE1786E)";
       services.jankyborders.inactive_color = "0x006EC3B4";
-      # Window manager
-      services.aerospace = {
-        enable = true;
-        settings = {
-            exec-on-workspace-change = [
-                "/bin/bash"
-                "-c"
-                "sketchybar --trigger aerospace_workspace_changed FOCUSED_WORKSPACE=$AEROSPACE_FOCUSED_WORKSPACE"
-            ];
-            gaps = {
-                outer.top        = 16;
-                outer.left       = 16;
-                outer.bottom     = 16;
-                outer.right      = 16;
-                inner.horizontal = 16;
-                inner.vertical   = 16;
-            };
-            on-window-detected = [
-                {
-                    check-further-callbacks = true;
-                    "if".window-title-regex-substring = "Launch";
-                    run = [ "layout floating" ];
-                }
-            ];
-            mode.main.binding = {
-                alt-h = "focus --boundaries-action wrap-around-the-workspace left";
-                alt-j = "focus --boundaries-action wrap-around-the-workspace down";
-                alt-k = "focus --boundaries-action wrap-around-the-workspace up";
-                alt-l = "focus --boundaries-action wrap-around-the-workspace right";
-                alt-shift-h = "move left";
-                alt-shift-j = "move down";
-                alt-shift-k = "move up";
-                alt-shift-l = "move right";
-                alt-ctrl-h = "join-with left";
-                alt-ctrl-j = "join-with down";
-                alt-ctrl-k = "join-with up";
-                alt-ctrl-l = "join-with right";
-                alt-a = "workspace 1";
-                alt-s = "workspace 2";
-                alt-d = "workspace 3";
-                alt-f = "workspace 4";
-                alt-g = "workspace 5";
-                alt-shift-a = "move-node-to-workspace 1";
-                alt-shift-s = "move-node-to-workspace 2";
-                alt-shift-d = "move-node-to-workspace 3";
-                alt-shift-f = "move-node-to-workspace 4";
-                alt-shift-g = "move-node-to-workspace 5";
-            };
-        };
-      };
     };
   in
   {
--- home.nix
+++ home.nix
@@ -1,30 +1,34 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "kevin.schmid";
-  home.homeDirectory = "/Users/kevin.schmid";
-  home.stateVersion = "25.05"; # Check home-manager release before changing
+    home.username = "kevin.schmid";
+    home.homeDirectory = "/Users/kevin.schmid";
+    home.stateVersion = "25.05"; # Check home-manager release before changing
 
-  programs.home-manager.enable = true;
-  home.shell.enableBashIntegration = true;
-  xdg.enable = true;
-  home.packages = with pkgs; [
-    opentofu
-    kubectl
-    kubelogin
-    kubelogin-oidc
-    kubernetes-helm
-  ];
-  home.shellAliases = {
-    
-  };
+    programs.home-manager.enable = true;
+    home.shell.enableBashIntegration = true;
+    xdg.enable = true;
+    home.packages = with pkgs; [
+        opentofu
+            kubectl
+            kubelogin
+            kubelogin-oidc
+            kubernetes-helm
+    ];
+    home.shellAliases = {
+        ls = "ls --color=auto";
+        l = "ls -lah";
+        v = "nvim";
+        g = "git";
+        k = "kubectl";
+    };
 
-  home.sessionVariables = {
-  };
+    home.sessionVariables = {
+    };
 
-  programs.firefox = {
-    enable = true;
-  };
+    programs.firefox = {
+        enable = true;
+    };
 
   programs.bash = {
     enable = true;
@@ -87,16 +91,116 @@
     userEmail = "schmid.kevin.manuel@gmail.com";
     userName = "Kevin Schmid";
   };
+      # Window manager
+  programs.aerospace = {
+      enable = true;
+      launchd.enable = true;
+      userSettings = {
+          after-startup-command = ["exec-and-forget ${pkgs.sketchybar}/bin/sketchybar"];
+          gaps = {
+              outer.top        = 16;
+              outer.left       = 16;
+              outer.bottom     = 16;
+              outer.right      = 16;
+              inner.horizontal = 16;
+              inner.vertical   = 16;
+          };
+          on-window-detected = [
+            {
+              check-further-callbacks = true;
+              "if".window-title-regex-substring = "Launch";
+              run = [ "layout floating" ];
+            }
+          ];
+          key-mapping.key-notation-to-key-code = {
+              mminus = "slash";
+              mplus = "rightSquareBracket";
+              moe = "semicolon";
+              mae = "quote";
+              mue = "leftSquareBracket";
+              mhash = "backslash";
+              msmaller = "backtick";
+              mbacktick = "equal";
+              msharps = "minus";
+              mcircumflex = "sectionSign";
+          };
+          mode.main.binding = {
+              alt-w = "mode window";
+          };
+          mode.window.binding = {
+              alt-c = "mode main";
+              h = "focus --boundaries-action wrap-around-the-workspace left";
+              j = "focus --boundaries-action wrap-around-the-workspace down";
+              k = "focus --boundaries-action wrap-around-the-workspace up";
+              l = "focus --boundaries-action wrap-around-the-workspace right";
+              shift-h = "move left";
+              shift-j = "move down";
+              shift-k = "move up";
+              shift-l = "move right";
+              "1" = "workspace 1";
+              "2" = "workspace 2";
+              "3" = "workspace 3";
+              "4" = "workspace 4";
+              "5" = "workspace 5";
+              shift-1 = "move-node-to-workspace 1";
+              shift-2 = "move-node-to-workspace 2";
+              shift-3 = "move-node-to-workspace 3";
+              shift-4 = "move-node-to-workspace 4";
+              shift-5 = "move-node-to-workspace 5";
+              mminus = "resize smart -50";
+              mplus = "resize smart +50";
+          };
+      };
+  };
+
+  programs.sketchybar = {
+    enable = true;
+    extraPackages = [
+        pkgs.aerospace
+    ];
+    config = ''
+PLUGIN_DIR="/Users/kevin.schmid/.config/sketchybar/plugins"
+${pkgs.sketchybar}/bin/sketchybar --bar position=top height=40 blur_radius=30 color=0xFF000000
+${pkgs.sketchybar}/bin/sketchybar --default \
+  padding_left=5 \
+  padding_right=5 \
+  icon.font="Cousine Nerd Font:Bold:17.0" \
+  label.font="Cousine Nerd Font:Bold:14.0" \
+  icon.color=0xffffffff \
+  label.color=0xffffffff \
+  icon.padding_left=4 \
+  icon.padding_right=4 \
+  label.padding_left=4 \
+  label.padding_right=4
+sketchybar --add event aerospace_workspace_change
+for sid in $(aerospace list-workspaces --all); do
+    ${pkgs.sketchybar}/bin/sketchybar --add item space.$sid left \
+        --subscribe space.$sid aerospace_workspace_change \
+        --set space.$sid \
+        label="$sid" \
+        click_script="aerospace workspace $sid" \
+        script="$PLUGIN_DIR/aerospace.sh $sid"
+done
+${pkgs.sketchybar}/bin/sketchybar --add item chevron left \
+           --set chevron label.drawing=off \
+           --add item front_app left \
+           --set front_app icon.drawing=off script="$PLUGIN_DIR/front_app.sh" \
+           --subscribe front_app front_app_switched
+${pkgs.sketchybar}/bin/sketchybar --add item clock right \
+           --set clock update_freq=10 icon=  script="$PLUGIN_DIR/clock.sh" \
+           --add item volume right \
+           --set volume script="$PLUGIN_DIR/volume.sh" \
+           --subscribe volume volume_change \
+           --add item battery right \
+           --set battery update_freq=120 script="$PLUGIN_DIR/battery.sh" \
+           --subscribe battery system_woke power_source_change
+${pkgs.sketchybar}/bin/sketchybar --update'';
+  };
 
   xdg.configFile."opentofu/tofurc" = {
     enable = true;
     text = ''
     plugin_cache_dir = "$XDG_CACHE_HOME/opentofu"'';
-  };
-  xdg.configFile.sketchybar = {
-    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nix/modules/sketchybar/config";
-    recursive = true;
-    force = true;
   };
 
   xdg.configFile.ghostty = {
